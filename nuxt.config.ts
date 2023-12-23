3319bb005af46e84d7deefc0a7db885af7480a4b
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-12-23 20:08:50 +0100

    chore: restore minification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  vite: {
-    build: {
-      minify: false,
-    },
-  },
   ssr: false,
   devtools: { enabled: true },
   modules: [
