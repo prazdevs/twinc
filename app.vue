fc7d65a99178808c0f9a987e20b97a1c74d87f7e
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-12-30 01:12:49 +0100

    feat: create pages

--- app.vue
+++ app.vue
@@ -1,29 +1,3 @@
-<script setup lang="ts">
-const channel = ref('vicksy')
-
-const { open, close, queue } = useChat(channel)
-</script>
-
 <template>
-  <div>
-    <input v-model="channel">
-    <button @click="open">
-      open
-    </button>
-    <button @click="() => close()">
-      close
-    </button>
-    <div>
-      <div v-for="m, i in queue" :key="i">
-        <b>{{ m.username }}</b><span>{{ m.message }}</span>
-      </div>
-    </div>
-  </div>
+  <NuxtPage />
 </template>
-
-<style scoped>
-body {
-  background-color: #1e1e2e;
-  color: white;
-}
-</style>
---
+++ pages/[channel].vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const route = useRoute()
+const channel = ref(route.params.channel as string)
+
+const { open, close, queue } = useChat(channel)
+</script>
+
+<template>
+  <div>
+    <input v-model="channel">
+    <button @click="open">
+      open
+    </button>
+    <button @click="() => close()">
+      close
+    </button>
+    <div>
+      <div v-for="m, i in queue" :key="i">
+        <b>{{ m.username }}</b><span>{{ m.message }}</span>
+        <div>
+          <pre>
+            {{ m.emotes }}
+          </pre>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-color: #1e1e2e;
+  color: white;
+}
+</style>
