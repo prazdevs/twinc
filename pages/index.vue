a9e82969c1f806fa43cc15f5dc6d0b83695ed031
prazdevs <1631886+prazdevs@users.noreply.github.com>
2024-01-01 19:13:13 +0100

    feat: index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const channel = ref('')
+</script>
+
+<template>
+  <div>
+    <label>
+      pick a channel to see chat
+      <input v-model="channel">
+    </label>
+    <NuxtLink :to="`/${channel}`">
+      open chat
+    </NuxtLink>
+  </div>
+</template>
