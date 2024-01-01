9eb1ecff53cee2ac882a419bfe0258da220bffce
prazdevs <1631886+prazdevs@users.noreply.github.com>
2024-01-01 19:13:03 +0100

    fix: anonymous id generator

--- utils/generators.ts
+++ utils/generators.ts
@@ -4,5 +4,5 @@ import { customAlphabet } from 'nanoid'
  * Generates an anonymous Twitch IRC username
  */
 export function generateUsername() {
-  return `justinfan${customAlphabet('1234567890', 6)}`
+  return `justinfan${customAlphabet('1234567890')(6)}`
 }
